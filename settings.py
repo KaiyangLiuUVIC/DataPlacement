ebe3af7661b09fe9f63760dc7e865feabd55126a
KaiyangLiuUVIC <37063936+KaiyangLiuUVIC@users.noreply.github.com>
2019-10-15 10:08:53 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,42 @@
+######SEED for all random
+CONST_SEED = 100008
+
+######Network Scale
+#node=16
+#node=21
+#node=26
+#node=31
+node=36
+
+data = 5000
+#data = 10000
+#data = 20000
+#data = 30000
+#data = 40000
+#data = 50000
+#data = 60000
+#data = 63731
+
+######weight
+w1=0.01                                                                        #Multi-get                                                             
+#w2=0.005
+w2=0.01
+#w2=0.015
+#w2=0.02
+#w2=0.025 
+
+######Read/write frequency
+epsion=1
+#epsion=19.2
+#epsion=19.2/5
+#epsion=19.2/10
+#epsion=19.2/15
+#epsion=19.2/20
+
+######Online Threshold
+#Theta=5
+Theta=10
+#Theta=15
+#Theta=20
+#Theta=25
+#Theta=30
